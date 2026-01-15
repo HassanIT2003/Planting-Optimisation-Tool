322257005cb2d467b0ba443b07031c13c01ace19
Hassan.noonari <133470148+HassanIT2003@users.noreply.github.com>
2026-01-15 16:06:11 +1100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,43 +1,26 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
-import packageJson from "./package.json";
-
-const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
-  } catch {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
-
-const fileName = {
-  es: `${getPackageName()}.js`,
-  iife: `${getPackageName()}.iife.js`,
-};
-
-const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 export default defineConfig({
   base: "./",
+
   build: {
     outDir: "./build/dist",
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        calculator: path.resolve(__dirname, 'calculator.html'),
-        recommendations: path.resolve(__dirname, 'recommendations.html'),
-        insights: path.resolve(__dirname, 'insights.html'),
+        main: path.resolve(__dirname, "index.html"),
+        calculator: path.resolve(__dirname, "calculator.html"),
+        recommendations: path.resolve(__dirname, "recommendations.html"),
+        insights: path.resolve(__dirname, "insights.html"),
       },
     },
   },
+
   test: {
     watch: false,
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -45,4 +28,3 @@ export default defineConfig({
     },
   },
 });
-
